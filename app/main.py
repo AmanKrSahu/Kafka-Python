d2fb6250e98350fc0c8cb5ad69a954044e1ea7c3
Aman <amankrsahu003@gmail.com>
2024-09-26 15:44:35 +0530

    Send Correlation ID

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 import socket  # noqa: F401
 
+# Create a message of 8 bytes(message length + correlation ID, 4 bytes each)
+def create_message(msg_len, corr_id):
+    return msg_len.to_bytes(4, byteorder="big") + corr_id.to_bytes(4, byteorder="big")
+
+# Handle the client connection
+def handle_client(client):
+    client.recv(1024)
+    client.sendall(create_message(7))
+    client.close()
+
 def main():
     # Print statement for debugging
     print("Logs from your program will appear here!")
 
     # Create a TCP server socket on localhost at port 9092 (common for Kafka)
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # Waiting for client to connect
+    
+    # Create an infinite loop waiting for incoming client connections
+    while True:
+        client, addr = server.accept()
+        handle_client(client)
 
 if __name__ == "__main__":
     main()
